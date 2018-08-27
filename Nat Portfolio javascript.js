d77248373096d7492d5267ae994131973c75e53d
Nat <40128337+natprogrammer33@users.noreply.github.com>
2018-08-27 07:11:50 -0400

    Update Nat Portfolio javascript.js
    Trying to get this javascript working

--- Nat Portfolio javascript.js
+++ Nat Portfolio javascript.js
@@ -1,6 +1,22 @@
-#test.onclick = function(){
-    x = 0
-    while(x != 10, x++){
-        window.scrolldown(0, 10);
-    }
-}
+var nav = document.querySelector('nav');
+var position = 0;
+
+// window.addEventListener('scroll', function(){
+//   if(window.pageYOffset > 100) {
+//     nav.classList += ' small';
+//   } else {
+//     nav.classList = 'main-nav';
+//   }
+// });
+
+window.addEventListener('scroll', function(){
+  if(position < window.pageYOffset) {
+    //console.log('down')
+    nav.classList += ' up';
+    position = window.pageYOffset;
+  } else {
+    //console.log('up');
+    nav.classList = 'nav small'
+    position = window.pageYOffset;
+  }
+})
