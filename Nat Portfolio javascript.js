cb2f3f3f3d70344337e3c3e30ca4f86e664d1350
Nat <40128337+natprogrammer33@users.noreply.github.com>
2018-10-14 21:35:26 -0400

    added scroll transitions

--- Nat Portfolio javascript.js
+++ Nat Portfolio javascript.js
@@ -1,6 +1,47 @@
-#test.onclick = function(){
-    x = 0
-    while(x != 10, x++){
-        window.scrolldown(0, 10);
-    }
+function contactscroll(){
+    window.scrollBy({
+        top: 21000,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function experiencescroll(){
+    window.scrollBy({
+        top: 1400,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function projectsscroll(){
+    window.scrollBy({
+        top: 900,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function aboutmescroll(){
+    window.scrollBy({
+        top: 100,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function scrolltotop(){
+    window.scrollBy({
+        top: -10000,
+        left:0,
+        behavior: 'smooth'
+    })
+}
+function getuserdata(){
+    var firstNameElement = document.getElementById('userfirstname');
+    var firstname = firstNameElement.value;
+    var lastNameElement = document.getElementById('userlastname');
+    var lastname = lastNameElement.value;
+    var emailElement=document.getElementById('useremail');
+    var email = emailElement.value;
+    alert("Thank you! I will be in contact shortly");
+    console.log(firstname);
+    console.log(lastname);
+    console.log(email);
 }
